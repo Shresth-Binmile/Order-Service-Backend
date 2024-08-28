48a96e1e9b4026505af89049b1986b50e2e07896
Shresth-Binmile <shresth.gupta@binmile.com>
2024-08-28 13:25:00 +0530

    Small Changes

--- src/routes/ordersRoutes.ts
+++ src/routes/ordersRoutes.ts
@@ -1,12 +1,9 @@
 import { Router } from "express";
 import { urls } from "../utils/enums";
 import createOrder from "../controllers/orderCreate";
-import updateOrder from "../controllers/orderUpdate";
 
 const router = Router()
 
 router.post(urls.ORDER_CREATE, createOrder)
-
-router.post(urls.ORDER_UPDATE, updateOrder)
 
 export default router
